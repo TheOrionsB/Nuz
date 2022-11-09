61cbf1e1ebe86195ce2a9fce72603202b9a882cf
Nathan <nathan.meneguz@epitech.eu>
2022-11-09 10:04:16 +0100

    [+] - SellingBullets component done

---
+++ src/components/Home/SellingBullets.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="border p-8 rounded space-y-2">
+        <h2 class="font-extrabold text-3xl text-white">Yet another url shortener but this time:</h2>
+        <ul class="font-bold text-2xl text-white pl-10">
+            <li>• Free</li>
+            <li>• Anonymous</li>
+            <li>• Encrypted</li>
+            <li>• Password protected</li>
+            <li>• With stats available</li>
+        </ul>
+    </div>
+</template>
+<script setup></script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,10 +1,13 @@
 <template>
     <div class="flex flex-col">
         <HeadingItemVue/>
-        <p>home</p>
+        <div class="flex flex-row px-10">
+            <SellingBulletsVue/>
+        </div>
     </div>
 </template>
 
 <script setup>
 import HeadingItemVue from '@/components/Home/HeadingItem.vue';
+import SellingBulletsVue from '@/components/Home/SellingBullets.vue';
 </script>
