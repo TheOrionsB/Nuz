eb6dfc428a49bd78b222f47b3c1a7e9c91be4e36
Nathan <nathan.meneguz@epitech.eu>
2022-11-12 00:05:28 +0100

    [+] - Sub component navigation working in dashboard

--- src/views/DashBoard.vue
+++ src/views/DashBoard.vue
@@ -7,14 +7,11 @@
                     <button class="text-red-300" @click="authStore.logout()">Log out</button>
                 </div>
                 <div class="bg-gray-900 border-purple-400 border-2 border-opacity-50 bg-opacity-70 text-2xl  class flex flex-col text-left">
-                    <button class="text-white py-4 px-8 w-full hover:bg-black hover:bg-opacity-50 text-left transition-all ease-in-out duration-150 first-of-type:rounded-t-xl">Home</button>
-                    <button class="text-white py-4 px-8 w-full hover:bg-black hover:bg-opacity-50 text-left transition-all ease-in-out duration-150">New link</button>
-                    <button class="text-white py-4 px-8 w-full hover:bg-black hover:bg-opacity-50 text-left transition-all ease-in-out duration-150">My links</button>
-                    <button class="text-white py-4 px-8 w-full hover:bg-black hover:bg-opacity-50 text-left transition-all ease-in-out duration-150  last-of-type:rounded-b-xl">Advanced</button>
+                    <button @click="currentComponentIndex = index" v-for="(subComponent, index) in subComponents" :key="index" class="text-white py-4 px-8 w-full hover:bg-black hover:bg-opacity-50 text-left transition-all ease-in-out duration-150 first-of-type:rounded-t-xl">{{subComponent.name}}</button>
                 </div>
             </div>
             <div class="bg-gray-900 border-2 bg-opacity-80 border-purple-400 border-opacity-40 w-3/4 ">
-                <NewLink/>
+                <component :is="subComponents[currentComponentIndex].component"/>
             </div>
         </div>
     </div>
@@ -23,6 +20,7 @@
 import { useAuthenticationStore } from '@/stores/AuthStore';
 import { storeToRefs } from 'pinia';
 import { useRouter } from 'vue-router';
+import { ref } from 'vue';
 import NewLink from '../components/Dashboard/NewLink.vue'
 
 const authStore = useAuthenticationStore();
@@ -30,4 +28,13 @@ const router = useRouter()
 const { username } = storeToRefs(authStore);
 if (username.value === "") router.push({ path: '/' });
 console.log(username.value);
+
+const currentComponentIndex = ref(0);
+
+const subComponents = [
+    {name: "Home", component: <div>Home</div>},
+    {name: "New link", component: <NewLink/>},
+    {name: "My links", component: <div>My links</div>},
+    {name: "Advanced", component: <div>Advanced</div>}
+]
 </script>
