650ce978bf40fa66cb65e9a17a04f4900b4430bb
Nathan <nathan.meneguz@epitech.eu>
2022-11-12 11:48:05 +0100

    [+] - Adding a 'Link name' field to the New link form

--- src/components/Dashboard/NewLink.vue
+++ src/components/Dashboard/NewLink.vue
@@ -2,23 +2,33 @@
     <div class="w-full h-full p-[5%] space-y-4">
         <div class="space-y-1">
             <h1 class="font-bold text-3xl">Create a new link</h1>
-            <p class="text-purple-300">If you don't want your link to expire or to be password protected, leave their optionnal fields empty.</p>
+            <p class="text-purple-300">If you don't want your link to expire or to be password protected, leave their
+                optionnal fields empty.</p>
         </div>
         <form class="flex flex-col space-y-4">
             <span class="flex flex-col space-y-1">
-                <label>Target</label>
+                <label>Link name</label>
                 <input
                     class="p-2 bg-black bg-opacity-40 rounded bg-transparent text-purple-300 border-2 border-purple-200 border-opacity-50"
-                    type="text" placeholder="Source URL" />
+                    type="text" placeholder="Name" />
             </span>
-            <span class="flex flex-row w-full items-center justify-center">
-                <span class="w-full flex flex-col justify-around space-y-1">
-                    <label class="w-1/4">Generated link</label>
-                    <span class="flex flex-row space-x-2">
-                        <input
-                            class="w-3/4 p-2 bg-black bg-opacity-40 rounded bg-transparent text-purple-300 border-2 border-purple-200 border-opacity-50"
-                            type="text" value="https://nuz.sh/t/Dz84" />
-                        <button class="w-1/4 rounded border-2 border-purple-200 border-opacity-50">I want anothter link</button>
+            <span class="flex flex-row space-x-4">
+                <span class="flex flex-col space-y-1 w-full">
+                    <label>Target URL</label>
+                    <input
+                        class="p-2 bg-black bg-opacity-40 rounded bg-transparent text-purple-300 border-2 border-purple-200 border-opacity-50"
+                        type="text" placeholder="URL" />
+                </span>
+                <span class="flex flex-row w-full items-center justify-center">
+                    <span class="w-full flex flex-col justify-around space-y-1">
+                        <label>Generated link</label>
+                        <span class="flex flex-row w-full space-x-2">
+                            <input
+                                class="p-2 bg-black w-2/3 bg-opacity-40 rounded bg-transparent text-purple-300 border-2 border-purple-200 border-opacity-50"
+                                type="text" value="https://nuz.sh/t/Dz84" />
+                            <button class="rounded w-1/3 border-2 border-purple-200 border-opacity-50">
+                                Regen</button>
+                        </span>
                     </span>
                 </span>
             </span>
