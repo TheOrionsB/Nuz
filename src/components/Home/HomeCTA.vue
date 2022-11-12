c6f3ec17346e559134a552a51445d3343ca68962
Nathan <nathan.meneguz@epitech.eu>
2022-11-12 11:55:57 +0100

    [+] - Adding dynamic rendering depending on authentication state in HomeCTA. Updated its test accordingly

--- src/components/Home/HomeCTA.vue
+++ src/components/Home/HomeCTA.vue
@@ -1,9 +1,20 @@
 <template>
-    <div class="flex flex-col justify-center items-center space-y-2 w-full">
-        <a name="mainCTA" href="/login?signup=1" class="text-3xl text-purple-300 border border-purple-300 rounded-xl p-5">{{ HomeStrings.CTA.mainCTA }}</a>
+    <div v-if="username === ''" class="flex flex-col justify-center items-center space-y-2 w-full">
+        <a name="mainCTA" href="/login?signup=1"
+            class="text-3xl text-purple-300 border border-purple-300 rounded-xl p-5">{{ HomeStrings.CTA.mainCTA }}</a>
         <a name="subCTA" href="/login" class="text-xl text-gray-500">{{ HomeStrings.CTA.subCTA }}</a>
+    </div>
+    <div v-if="username !== ''" class="flex flex-col justify-center items-center space-y-2 w-full">
+        <h2 name="mainCTA" class="text-3xl text-purple-300 p-2">Glad to see you back {{ username }} !</h2>
+        <a name="subCTA" href="/login" class="text-xl text-gray-500">Go to your dashboard</a>
     </div>
 </template>
 <script setup>
+import { storeToRefs } from 'pinia';
 import HomeStrings from '../../assets/homestrings.json'
+import { useAuthenticationStore } from '../../stores/AuthStore';
+
+const authStore = useAuthenticationStore();
+
+const { username } = storeToRefs(authStore);
 </script>
--- src/components/__test__/Home/HomeCTA.spec.js
+++ src/components/__test__/Home/HomeCTA.spec.js
@@ -1,13 +1,18 @@
 import { mount } from '@vue/test-utils';
 import HomeCTA from '../../Home/HomeCTA.vue';
-import { describe, expect, it, vi } from "vitest";
+import { beforeEach, describe, expect, it, vi } from "vitest";
 import HomeStrings from '../../../assets/homestrings.json';
+import { createPinia, setActivePinia } from 'pinia'
 
 vi.mock('vue-router', () => ({
     resolve: vi.fn(),
+    useRouter: vi.fn()
 }));
 
 describe('HomeCTA', () => {
+    beforeEach(() => {
+        setActivePinia(createPinia());
+    })
     it('Renders properly', () => {
         const wrapper = mount(HomeCTA)
         expect(wrapper.get("a[name='mainCTA']").text()).toContain(HomeStrings.CTA.mainCTA)
