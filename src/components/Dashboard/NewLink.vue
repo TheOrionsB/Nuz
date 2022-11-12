0039a5391f85533e5abee1547636c6877b2a692b
Nathan <nathan.meneguz@epitech.eu>
2022-11-12 11:30:15 +0100

    [~] - Clearly marking optionnal fields as '(Optionnal). Dashboard subcomponent navigation now highlights the active subcomponent'

--- src/components/Dashboard/NewLink.vue
+++ src/components/Dashboard/NewLink.vue
@@ -2,34 +2,34 @@
     <div class="w-full h-full p-[5%] space-y-4">
         <div class="space-y-1">
             <h1 class="font-bold text-3xl">Create a new link</h1>
-            <p class="text-purple-300">Fields marked as "?" are optionnal. If you don't want your link to expire or to be password protected, leave them empty.</p>
+            <p class="text-purple-300">If you don't want your link to expire or to be password protected, leave their optionnal fields empty.</p>
         </div>
         <form class="flex flex-col space-y-4">
             <span class="flex flex-col space-y-1">
-                <label>Input</label>
+                <label>Target</label>
                 <input
                     class="p-2 bg-black bg-opacity-40 rounded bg-transparent text-purple-300 border-2 border-purple-200 border-opacity-50"
                     type="text" placeholder="Source URL" />
             </span>
             <span class="flex flex-row w-full items-center justify-center">
                 <span class="w-full flex flex-col justify-around space-y-1">
-                    <label class="w-1/4">Output</label>
+                    <label class="w-1/4">Generated link</label>
                     <span class="flex flex-row space-x-2">
                         <input
                             class="w-3/4 p-2 bg-black bg-opacity-40 rounded bg-transparent text-purple-300 border-2 border-purple-200 border-opacity-50"
                             type="text" value="https://nuz.sh/t/Dz84" />
-                        <button class="w-1/4 rounded border-2 border-purple-200 border-opacity-50">Regen link</button>
+                        <button class="w-1/4 rounded border-2 border-purple-200 border-opacity-50">I want anothter link</button>
                     </span>
                 </span>
             </span>
             <span class="flex flex-col space-y-1">
-                <label>Password protected ?</label>
+                <label>Password protected (Optionnal)</label>
                 <input
                     class="w-full p-2 bg-black bg-opacity-40 rounded bg-transparent text-purple-300 border-2 border-purple-200 border-opacity-50"
                     type="text" placeholder="Password" />
             </span>
             <span class="flex flex-col space-y-1">
-                <label>Expiration ?</label>
+                <label>Expiration (Optionnal)</label>
                 <input
                     class="w-full p-2 bg-black bg-opacity-40 rounded bg-transparent text-purple-300 border-2 border-purple-200 border-opacity-50"
                     type="datetime-local" placeholder="Password" />
