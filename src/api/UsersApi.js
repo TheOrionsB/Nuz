05b1754f0390ae11c928ea33dc49898e84df2d2d
Nathan <nathan.meneguz@epitech.eu>
2022-11-14 12:51:41 +0100

    [-~] - Removing getShorten method from UsersApi.js, moved to ShortenApi.js. Removing all 'http://' in fetch requests that resulted in an unresolvable host

--- src/api/UsersApi.js
+++ src/api/UsersApi.js
@@ -29,20 +29,14 @@ export const authenticate = async (inputs, action) => {
     }
 }
 
-export const getShortened = async (username, token) => {
-    const response = await fetch(`http://${process.env.VUE_APP_API_ENDPOINT}/user/${username}`, { method: "GET", headers: { 'authorization': `Bearer ${token}` } });
-    if (response.status === 200) return (response.data);
-    return (null);
-}
-
 export const deleteAccount = async (username, token) => {
-    const response = await fetch(`http://${process.env.VUE_APP_API_ENDPOINT}/user/${username}`, { method: "DELETE", headers: { 'authorization': `Bearer ${token}` } })
+    const response = await fetch(`${process.env.VUE_APP_API_ENDPOINT}/user/${username}`, { method: "DELETE", headers: { 'authorization': `Bearer ${token}` } })
     if (response.status === 200) return (true);
     return (false);
 }
 
 export const updatePassword = async (username, newPassword, token) => {
-    const response = await fetch(`http://${process.env.VUE_APP_API_ENDPOINT}/user`, {
+    const response = await fetch(`${process.env.VUE_APP_API_ENDPOINT}/user`, {
         method: "PUT", headers: { 'authorization': `Bearer ${token}` }, body: {
             password: newPassword
         }
