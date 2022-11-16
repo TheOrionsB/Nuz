17dd22e59e544463de5763ce8c31180e6327ef09
Nathan <nathan.meneguz@epitech.eu>
2022-11-16 11:30:15 +0100

    [~] - Solving warnings during unit testing with stubs and correct pinia test

--- src/components/__test__/HeaderGlobal.spec.js
+++ src/components/__test__/HeaderGlobal.spec.js
@@ -1,21 +1,26 @@
-import { mount } from '@vue/test-utils';
+import { mount, RouterLinkStub } from '@vue/test-utils';
 import HeaderGlobalVue from '../HeaderGlobal.vue';
-import { beforeEach, describe, expect, it, vi } from "vitest";
+import { describe, expect, it, vi } from "vitest";
 import HomeStrings from '../../assets/homestrings.json';
-import { setActivePinia, createPinia } from 'pinia'
+import { createTestingPinia } from '@pinia/testing'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 
 vi.mock('vue-router', () => ({
     resolve: vi.fn(),
     useRouter: vi.fn()
-  }));
+}));
 
 describe('HeaderGlobal', () => {
-    beforeEach(() => {
-        setActivePinia(createPinia())
-    })
     it('Renders properly', () => {
-        const wrapper = mount(HeaderGlobalVue);
-        expect(wrapper.text()).toContain(HomeStrings.headerTitle);
+        const wrapper = mount(HeaderGlobalVue, {
+            global: {
+                plugins: [createTestingPinia({ createSpy: vi.fn })],
+                stubs: {
+                    FontAwesomeIcon: FontAwesomeIcon,
+                    RouterLink: RouterLinkStub
+                }
+            }
+        }); expect(wrapper.text()).toContain(HomeStrings.headerTitle);
         for (let i in HomeStrings.Navigation.length) {
             expect(wrapper.find(`a[key=${i}]`).text()).toContain(HomeStrings.Navigation[i].pretty);
         }
--- src/components/__test__/Home/HomeCTA.spec.js
+++ src/components/__test__/Home/HomeCTA.spec.js
@@ -1,8 +1,8 @@
 import { mount } from '@vue/test-utils';
 import HomeCTA from '../../Home/HomeCTA.vue';
-import { beforeEach, describe, expect, it, vi } from "vitest";
+import { describe, expect, it, vi } from "vitest";
 import HomeStrings from '../../../assets/homestrings.json';
-import { createPinia, setActivePinia } from 'pinia'
+import { createTestingPinia } from '@pinia/testing'
 
 vi.mock('vue-router', () => ({
     resolve: vi.fn(),
@@ -10,12 +10,14 @@ vi.mock('vue-router', () => ({
 }));
 
 describe('HomeCTA', () => {
-    beforeEach(() => {
-        setActivePinia(createPinia());
-    })
+
     it('Renders properly', () => {
-        const wrapper = mount(HomeCTA)
-        expect(wrapper.get("a[name='mainCTA']").text()).toContain(HomeStrings.CTA.mainCTA)
+        const wrapper = mount(HomeCTA, {
+            global: {
+                plugins: [ createTestingPinia({ createSpy: vi.fn }) ],
+                stubs: ['FontAwesomeIcon']
+            }
+        });        expect(wrapper.get("a[name='mainCTA']").text()).toContain(HomeStrings.CTA.mainCTA)
         expect(wrapper.get("a[name='subCTA']").text()).toContain(HomeStrings.CTA.subCTA)
     })
 })
