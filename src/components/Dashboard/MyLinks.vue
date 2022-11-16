a9adc2890517a8278de2229fac5361ca0ff1ff6a
Nathan <nathan.meneguz@epitech.eu>
2022-11-16 10:44:53 +0100

    [+] - Adding copy to clipboard button in MyLinks

--- src/components/Dashboard/MyLinks.vue
+++ src/components/Dashboard/MyLinks.vue
@@ -11,13 +11,16 @@
                     v-for="(item, index) in shortenedList" :key="index">
                     <div class="flex flex-row w-2/3 justify-start space-x-4">
                         <p class="w-1/3">{{ item.name }}</p>
-                        <button class="w-1/3 underline text-purple-500">Access statistics</button>
+                        <a class="w-1/3 underline text-purple-500" v-bind:href="genFullLink(item.source)">{{
+                                genFullLink(item.source)
+                        }}</a>
                     </div>
-                    <div class="space-x-2 class flex flex-row w-1/3">
+                    <div class="space-x-2 class flex flex-row w-2/5">
+                        <button @click="copyToClipBoard(genFullLink(item.source))"
+                            class="border-gray-800 border p-2 bg-indigo-700 text-indigo-300 hover:bg-indigo-900 hover:text-indigo-200 ease-in-out duration-200 rounded w-full">Copy
+                            link</button>
                         <button
                             class="border-gray-800 border p-2 bg-purple-700 text-purple-300 hover:bg-purple-900 hover:text-purple-200 ease-in-out duration-200 rounded w-full">Edit</button>
-                        <button
-                            class="border-gray-800 border p-2 bg-orange-700 text-orange-300 hover:bg-orange-900 hover:text-orange-200 ease-in-out duration-200 rounded w-full">Expire</button>
                         <button
                             class="border-gray-800 border p-2 bg-red-700 text-red-300 hover:bg-red-900 hover:text-red-200 ease-in-out duration-200 rounded w-full">Delete</button>
                     </div>
@@ -34,6 +37,13 @@ const searchInput = ref(null);
 const searchRef = ref(null);
 const shortenedList = ref([]);
 
+const genFullLink = (suffix) => {
+    return `${process.env.VUE_APP_REDIRECTION_BASEURL}/${suffix}`;
+}
+const copyToClipBoard = (item) => {
+    navigator.clipboard.writeText(item)
+}
+
 onMounted(async () => {
     document.addEventListener('keydown', (event) => {
         if (event.metaKey) {
@@ -43,7 +53,7 @@ onMounted(async () => {
                 }
             })
         }
-    })
-    shortenedList.value = (await getShortened()).shortened.reverse();
+    });
+    shortenedList.value = await (await getShortened()).shortened
 })
 </script>
