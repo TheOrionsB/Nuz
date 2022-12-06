ec49e0db8983de4a2beb0b36a5ad925315c29af7
Nathan <44578736+TheOrionsB@users.noreply.github.com>
2022-12-06 18:53:53 +0100

    [~] - Editing home view repo links

--- src/components/Home/UnsignedShortener.vue
+++ src/components/Home/UnsignedShortener.vue
@@ -3,8 +3,8 @@
         <h2 class="max-lg:text-xl lg:text-3xl font-bold text-white ">Having issues or want to contribute ?</h2>
         <p class="max-lg:text-lg lg:text-2xl text-purple-400">Nuz.sh is an open source project ! You can contribute by forking, contributing or submit an issue on its github repository!</p>
         <div class="text-white flex flex-row space-x-4 max-lg:w-full lg:w-1/3 justify-center">
-            <a class="flex flex-row justify-center items-center max-lg:text-sm lg:text-2xl bg-purple-500 p-3 rounded w-1/2 space-x-2" href='https://nuz.sh/PJNkm179'><font-awesome-icon :icon="['fab', 'github']"/><p>Submit an issue</p></a>
-            <a class="flex flex-row justify-center items-center max-lg:text-sm lg:text-2xl bg-purple-500 p-3 rounded w-1/2 space-x-2" href='https://nuz.sh/GiJQT199'><font-awesome-icon :icon="['fab', 'github']"/><p>Contribute</p></a>
+            <a class="flex flex-row justify-center items-center max-lg:text-sm lg:text-2xl bg-purple-500 p-3 rounded w-1/2 space-x-2" href='https://nuz.sh/82rtw682'><font-awesome-icon :icon="['fab', 'github']"/><p>Submit an issue</p></a>
+            <a class="flex flex-row justify-center items-center max-lg:text-sm lg:text-2xl bg-purple-500 p-3 rounded w-1/2 space-x-2" href='https://nuz.sh/NAGto653'><font-awesome-icon :icon="['fab', 'github']"/><p>Contribute</p></a>
         </div>
     </div>
 </template>
